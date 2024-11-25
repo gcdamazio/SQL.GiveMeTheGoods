eecb9c26e67b70ca0f0be141f8004c405e92e706
Gabrielle Damazio <damaziogabrielle@gmail.com>
2024-11-24 22:20:11 -0500

    DONE exercise12.sql

---
+++ answers/exercise12.sql
@@ -0,0 +1,5 @@
+Use the IN operator to select all the records from the Students table where Country is NOT "Sint Maarten" and NOT "Haiti".
+
+SELECT *
+FROM Students
+WHERE Country NOT IN ('Sint Maarten','Haiti');
